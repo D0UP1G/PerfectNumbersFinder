7a5b32f5cd3381d08dd4e41820ab06d54da8cab8
DO U PIG <113414349+D0UP1G@users.noreply.github.com>
2024-07-06 20:34:02 +0100

    v6

--- main.py
+++ main.py
@@ -1,27 +1,37 @@
-import logging
-import sys
-sys.set_int_max_str_digits(10000)
+import logging 
+import sys 
+sys.set_int_max_str_digits(2147483647)
 
-x = 0
+x = 26926
 
+logging.basicConfig(filename='logs.log', level=logging.INFO)
 
-logging.basicConfig(filename="logs.log",level=logging.INFO)
+class Number():
+    def __init__(self, i:int, is_perfect:bool = False):
+        self.checked = 2**i-1
+        self.is_perfect = is_perfect
+import random
+def fermat_test(n, k=100):
+	if n <= 1:
+		return False
+	if n % 2 == 0:
+		return False
+	for _ in range(k):
+		a = random.randint(1, n-1)
+		if pow(a, n-1, n) != 1:
+			return False
+		return True  
 
-def PerfectNumGenerator(x):
-    y = 0
-    for i in range(1,x+1):
-        if x%i == 0:
-            y+=i
-    if y == x+1:
-        res = 2**(x-1) * (2**x-1)
-        logging.info(f"{x}: {res}")
-        return res
-        
-    else:
-        return False
+def find_perfect_number(i:int):
+    Xnum = Number(i)
+    if Xnum.is_perfect != True:
+        if fermat_test(Xnum.checked):
+            Xnum.is_perfect = True
+            
+    return Xnum.is_perfect
 
 while True:
-    x+=1
-    perfect = PerfectNumGenerator(x)
-    if perfect:
-        print(f'I FOUND NEW PERFECT NUMBER!: {perfect}')
+    print(x)
+    x+= 1
+    if find_perfect_number(x):
+        logging.info(f"p:{x}")
